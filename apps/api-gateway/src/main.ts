ce070aa78a2e9eda8f8e3e59da025b3de922af8a
Martin Emil <mar10martinemil@gmail.com>
2025-03-19 18:09:39 +0200

    added swagger for documentation and testing api

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { ApiGatewayModule } from './api-gateway.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApiGatewayModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Easygenerator API')
+    .setVersion('1.0')
+    .setDescription('A test API for easygenerator fullstack engineer position')
+    .build();
+
+  const documentFactory = () =>
+    SwaggerModule.createDocument(app, config, {
+      autoTagControllers: true,
+    });
+
+  SwaggerModule.setup('/api-swagger', app, documentFactory);
+
   await app.listen(process.env.port ?? 3000);
 }
 bootstrap();
