466ff5acce42cb76209fdb593347c955b6099174
Martin Emil <mar10martinemil@gmail.com>
2025-03-20 16:43:52 +0200

    added username length validation

--- apps/api-gateway/src/users/dto/create-user.dto.ts
+++ apps/api-gateway/src/users/dto/create-user.dto.ts
@@ -1,9 +1,15 @@
 import { CreateUserDto } from '@easygen/proto/auth';
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsString, IsStrongPassword } from 'class-validator';
+import {
+  IsEmail,
+  IsString,
+  IsStrongPassword,
+  MinLength,
+} from 'class-validator';
 
 export class CreateUserApiDto implements CreateUserDto {
   @IsString()
+  @MinLength(3)
   @ApiProperty()
   username: string;
 
--- apps/api-gateway/src/users/dto/update-user.dto.ts
+++ apps/api-gateway/src/users/dto/update-user.dto.ts
@@ -1,9 +1,10 @@
 import { UpdateUserDto } from '@easygen/proto/auth';
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsOptional, IsString } from 'class-validator';
+import { IsEmail, IsOptional, IsString, MinLength } from 'class-validator';
 
 export class UpdateUserApiDto implements Partial<UpdateUserDto> {
   @IsString()
+  @MinLength(3)
   @IsOptional()
   @ApiProperty({ required: false })
   username: string;
