409aecb233441943030bd3fec60ab6a9afefc4cb
Martin Emil <mar10martinemil@gmail.com>
2025-03-25 20:56:02 +0200

    added general rate limiter

--- apps/api-gateway/src/api-gateway.module.ts
+++ apps/api-gateway/src/api-gateway.module.ts
@@ -1,9 +1,18 @@
 import { SysConfigsModule } from '@easygen/sys-configs';
 import { Module } from '@nestjs/common';
+import { seconds, ThrottlerModule } from '@nestjs/throttler';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, SysConfigsModule],
+  imports: [
+    ThrottlerModule.forRoot({
+      // general rate limit as testing the endpoints will most propably break all rate limits
+      throttlers: [{ ttl: seconds(60), limit: 10 }],
+    }),
+    AuthModule,
+    UsersModule,
+    SysConfigsModule,
+  ],
 })
 export class ApiGatewayModule {}
